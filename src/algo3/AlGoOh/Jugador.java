7008d49bd07ff16cc2092dc5a281f1952c16b279
gero17illescas <gero17illescas@gmail.com>
2018-06-27 00:49:16 -0300

    Estado implementado

--- src/algo3/AlGoOh/Jugador.java
+++ src/algo3/AlGoOh/Jugador.java
@@ -113,6 +113,9 @@ public class Jugador {
 
 	public void reducirVida(int danio) {
 		puntosDeVida -= danio;
+		if(puntosDeVida <= 0) {
+			oponente.declararComoGanador();
+		}
 	}
 	
 	public void tomarUnaCartaDelMazo() {
