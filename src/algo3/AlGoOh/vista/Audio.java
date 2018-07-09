6e56ad500aff8313ca64f648b475a120ee3cfe35
gero_ <gero_@GeroIllescas>
2018-07-09 14:42:32 -0300

    Refactorizacion en Audio

--- src/algo3/AlGoOh/vista/Audio.java
+++ src/algo3/AlGoOh/vista/Audio.java
@@ -9,33 +9,26 @@ public class Audio {
 	private static MediaPlayer mediaPlayer;
 
 	public static void reproducirAlerta() {
-		String path = "src/algo3/AlGoOh/audio/alerta.mp3";
-        Media media = new Media(new File(path).toURI().toString());
-        MediaPlayer mediaPlayerAlerta = new MediaPlayer(media);
-        mediaPlayerAlerta.play();
+		reproducir("src/algo3/AlGoOh/audio/alerta.mp3");
 	}
 	
 	public static void reproducirIntro() {
-        String path = "src/algo3/AlGoOh/audio/intro.mp3";
-        Media media = new Media(new File(path).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
+        reproducir("src/algo3/AlGoOh/audio/intro.mp3");;
 	}
 	
 	public static void reproducirBatalla() {
 		mediaPlayer.pause();
-        String path = "src/algo3/AlGoOh/audio/Batalla.mp3";
-        Media media = new Media(new File(path).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
+        reproducir("src/algo3/AlGoOh/audio/Batalla.mp3");
 	}
 	
 	public static void reproducirVictoria() {
 		mediaPlayer.pause();
-        String path = "src/algo3/AlGoOh/audio/victoria.mp3";
-        Media media = new Media(new File(path).toURI().toString());
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setAutoPlay(true);
+        reproducir("src/algo3/AlGoOh/audio/victoria.mp3");
+	}
+	private static void reproducir (String path) {
+		 Media media = new Media(new File(path).toURI().toString());
+	     mediaPlayer = new MediaPlayer(media);
+	     mediaPlayer.setAutoPlay(true);
 	}
 	
 }
